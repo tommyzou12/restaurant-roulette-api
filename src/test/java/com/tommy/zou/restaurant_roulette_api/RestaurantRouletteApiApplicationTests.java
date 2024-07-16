733153e177f58dd2c7d837d342b08548e20e684f
Tommy Zou <tommyzou12@gmail.com>
2024-07-15 21:09:10 -0400

    initializing project

---
+++ src/test/java/com/tommy/zou/restaurant_roulette_api/RestaurantRouletteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.zou.restaurant_roulette_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRouletteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
